0f36e32e69c94059574fe2d13e9da61cf1aaf190
sriram <sriram199916@gmail.com>
2020-10-11 13:40:25 +0530

    Updates

---
+++ precache-manifest.00b63d2df73f551dc048bf53559b4c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7e0dd1501efd4b3f509",
+    "url": "/social-network/static/css/main.851d80b8.chunk.css"
+  },
+  {
+    "revision": "f7e0dd1501efd4b3f509",
+    "url": "/social-network/static/js/main.f7e0dd15.chunk.js"
+  },
+  {
+    "revision": "a30dbec79177916856ed",
+    "url": "/social-network/static/css/1.ed5c55c0.chunk.css"
+  },
+  {
+    "revision": "a30dbec79177916856ed",
+    "url": "/social-network/static/js/1.a30dbec7.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "6af513c08ec165e4f9c8e2c849222a60",
+    "url": "/social-network/index.html"
+  }
+];
